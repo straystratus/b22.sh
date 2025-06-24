72721f93a8e3d1be4b0c228799b48397e8422cb7
stratus <straystratus@users.noreply.github.com>
2025-06-24 11:55:07 +0200

    modif

--- src/aboutme/stratus/food/food.js
+++ src/aboutme/stratus/food/food.js
@@ -1,5 +1,11 @@
 window.food = {
-  fav: "tea\nrice\nchicken",
-  dislike: "oysters\nmushrooms",
-  breakfast: window.breakfast
+  fav: `tea
+rice
+chicken`,
+  dislike: `oysters
+mushrooms`,
 };
+
+registerFile("aboutme/stratus/food/fav", "Text: food.fav");
+registerFile("aboutme/stratus/food/dislike", "Text: food.dislike");
+registerFile("aboutme/stratus/food/breakfast", "Command: breakfast");
