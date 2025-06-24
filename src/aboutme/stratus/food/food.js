6976eae8cd757e1b3c547c58750ad64a4250317c
stratus <straystratus@users.noreply.github.com>
2025-06-24 22:58:42 +0200

    adding not corrupted pictures..

--- src/aboutme/stratus/food/food.js
+++ src/aboutme/stratus/food/food.js
@@ -10,8 +10,8 @@ mushrooms`,
 
 here's what i eat in the morning (around 8am)
 
-<img src="/img/breakfast_mix.png" width="300">
-<img src="/img/breakfast_mix2.png" width="300">
+<img src="/img/breakfast_mix.jpg" width="300">
+<img src="/img/breakfast_mix2.jpg" width="300">
 `
 };
 
