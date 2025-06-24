340e749d4f162d56bcf8e712e2850945be3bde27
stratus <straystratus@users.noreply.github.com>
2025-06-24 11:32:25 +0200

    test

---
+++ src/aboutme/stratus/food/breakfast.js
@@ -0,0 +1,9 @@
+registerCommand("breakfast", () => {
+  return `
+🍳 <strong>breakfast time!</strong>
+
+here's what i eat in the morning (around 8am)
+
+<a href="https://postimg.cc/hXBnXTq8" target="_blank"></a>
+<a href="https://postimg.cc/y3dSMxPt" target="_blank"></a>`
+});
