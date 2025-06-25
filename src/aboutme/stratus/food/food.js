89c392271c3a50559416941e4a780eb03c817b1a
stratus <straystratus@users.noreply.github.com>
2025-06-25 20:01:27 +0200

    reformating?

--- src/aboutme/stratus/food/food.js
+++ src/aboutme/stratus/food/food.js
@@ -11,8 +11,7 @@ mushrooms`,
 this is my morning nuts mix.
 i add cottage cheese with it.
 
-<img src="public/img/breakfast_mix.jpg" width="300">
-<img src="public/img/breakfast_mix2.jpg" width="300">
+<img src="public/img/breakfast_mix.jpg" width="300"> <img src="public/img/breakfast_mix2.jpg" width="300">
 
 here are the macros and the cost for an average bowl.
 (note that i tend to add fruits, like banana, peach, strawberry...)
