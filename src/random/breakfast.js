faece2e2c83c90c6d870db5808acd82bab1249da
stratus <straystratus@users.noreply.github.com>
2025-06-25 21:20:18 +0200

    new try

--- src/random/breakfast.js
+++ src/random/breakfast.js
@@ -1,16 +1,18 @@
-window.breakfast = `
-<strong>breakfast time!</strong>
+window.random = {
+  breakfast:
+  `<strong>breakfast time!</strong>
 
-this is my morning nuts mix.
-i add cottage cheese with it.
+  this is my morning nuts mix.
+  i add cottage cheese with it.
 
-<img src="public/img/breakfast_mix.jpg" width="300"> <img src="public/img/breakfast_mix2.jpg" width="300">
+  <img src="public/img/breakfast_mix.jpg" width="300"> <img src="public/img/breakfast_mix2.jpg" width="300">
 
-here are the macros and the cost for an average bowl.
-(note that i tend to add fruits, like banana, peach, strawberry...)
-(i sometimes add some more dried fruits like mango too)
-<img src="public/img/breakfast_tab.png" width="500">
+  here are the macros and the cost for an average bowl.
+  (note that i tend to add fruits, like banana, peach, strawberry...)
+  (i sometimes add some more dried fruits like mango too)
+  <img src="public/img/breakfast_tab.png" width="500">
 
-the goal is to be 50% full.
-i generaly eat this with a matcha but i like to change sometimes for a sencha or genmaicha.
-`;
+  the goal is to be 50% full.
+  i generaly eat this with a matcha but i like to change sometimes for a sencha or genmaicha.
+`
+};
