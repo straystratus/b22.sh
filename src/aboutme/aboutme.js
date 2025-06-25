8d3899f5637ed5dc7af9d6c3b25f423842d58f79
stratus <straystratus@users.noreply.github.com>
2025-06-25 20:39:39 +0200

    reformating

--- src/aboutme/aboutme.js
+++ src/aboutme/aboutme.js
@@ -1,5 +1,10 @@
-"hey ! im stratus, but you can call me wheat. im actively working on zksnarks, ethereum developement and C, of course.\ni like birds and tea, a lot."
-window.aboutme = {
-  contacts: window.contacts,
-  stratus: window.stratus
-};
+window.aboutme = `
+"hey ! im stratus, but you can call me wheat.
+im actively working on zksnarks, ethereum developement and C, of course.
+i like birds and tea, a lot.
+
+contact me :
+<a href="https://twitter.com/stratus_eth" target="_blank">twitter.com/stratus_eth</a>
+<a href="https://github.com/straystratus" target="_blank">github.com/straystratus</a>
+<a href="mailto:stratus@pm.me">stratus@pm.me</a>
+`;
--- src/aboutme/contacts.js
+++
@@ -1,4 +0,0 @@
-window.contacts =
-  `<a href="mailto:straystratus@gmail.com">email</a>\n` +
-  `<a href="https://github.com/straystratus">github</a>\n` +
-  `<a href="https://x.com/stratustm">twitter</a>`;
