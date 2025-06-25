92326c67bb4c7a2fabd171d3989a96bb42423bf6
stratus <straystratus@users.noreply.github.com>
2025-06-25 20:44:24 +0200

    tiny modif

--- src/aboutme/aboutme.js
+++ src/aboutme/aboutme.js
@@ -1,10 +1,10 @@
 window.aboutme = `
-"hey ! im stratus, but you can call me wheat.
+hey ! im stratus, but you can call me wheat.
 im actively working on zksnarks, ethereum developement and C, of course.
 i like birds and tea, a lot.
 
 contact me :
-<a href="https://twitter.com/stratus_eth" target="_blank">twitter.com/stratus_eth</a>
-<a href="https://github.com/straystratus" target="_blank">github.com/straystratus</a>
-<a href="mailto:stratus@pm.me">stratus@pm.me</a>
+<a href="https://twitter.com/stratustm" target="_blank">X</a>
+<a href="https://github.com/straystratus" target="_blank">github</a>
+<a href="mailto:straystratus@gmail.com">mail</a>
 `;
