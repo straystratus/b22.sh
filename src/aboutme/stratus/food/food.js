3df2eef2f8b8e48d1e90de2fd769cb198eec2336
stratus <straystratus@users.noreply.github.com>
2025-06-25 19:52:29 +0200

    breakfast update

--- src/aboutme/stratus/food/food.js
+++ src/aboutme/stratus/food/food.js
@@ -8,10 +8,19 @@ mushrooms`,
 
 <strong>breakfast time!</strong>
 
-here's what i eat in the morning (around 8am)
+this is my morning nuts mix.
+i add cottage cheese with it.
 
 <img src="public/img/breakfast_mix.jpg" width="300">
 <img src="public/img/breakfast_mix2.jpg" width="300">
+
+here are the macros and the cost for an average bowl.
+(note that i tend to add fruits, like banana, peach, strawberry...)
+(i sometimes add some more dried fruits like mango too)
+<img src="public/img/breakfast_tab.png" width="300">
+
+the goal is to be 50% full.
+i generaly eat this with a matcha but i like to change sometimes for a sencha or genmaicha.
 `
 };
 
