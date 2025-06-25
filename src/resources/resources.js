17c0b1cf608cfd0dc3e879487c91a11799d1e5f4
stratus <straystratus@users.noreply.github.com>
2025-06-25 21:10:13 +0200

    optimizing

--- src/resources/resources.js
+++ src/resources/resources.js
@@ -6,4 +6,10 @@ window.resources = {
     `- <a href="https://www.furidamu.org/" target="_blank">Julian Schrittwieser</a>\n` +
     `- <a href="https://near.blog/" target="_blank">near.blog</a>\n` +
     `- <a href="https://riemani.ca/" target="_blank">mani</a>\n`,
+  readings:
+    `- <a href="http://www.catb.org/esr/faqs/hacker-howto.html#hacker_already" target="_blank">How To Become A Hacker</a>\n` +
+    `- <a href="https://www.feynmanlectures.caltech.edu/" target="_blank">The Feynman Lectures on Physics</a>\n` +
+    `- <a href="https://people.freebsd.org/~lstewart/articles/cpumemory.pdf" target="_blank">What Every Programmer Should Know About Memory</a>\n` +
+    `- <a href="https://web.mit.edu/6.001/6.037/sicp.pdf" target="_blank">SICP</a>\n` +
+    `- <a href="https://oceanofpdf.com/authors/lukas-hozda/pdf-building-bitcoin-in-rust-download/" target="_blank">Building Bitcoin in Rust</a>`
 };
